3fb96c7d718ab986d03d17884d0e0ca0788ff270
Patrik Rizell <patrik.rizell@tunstall.com>
2023-04-18 18:22:15 +0200

    Made it easier to run in Dev. Changed BorderColor

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -9,10 +9,11 @@ const Container = ({ children, isFocusTime = true }: Props) => {
   const bgColor = isFocusTime
     ? "bg-gradient-to-br from-focus from-30% to-focus-dark"
     : "bg-gradient-to-br from-break from-30% to-break-dark ";
+  const borderColor = isFocusTime ? "border-focus-dark" : "border-break-dark";
   return (
     <div
       className={`w-96 h-96 max-w-md p-24 flex flex-col items-center justify-center text-primary relative shadow-lg ${bgColor} 
-      border-2 border-black`}
+      border-2 ${borderColor}`}
     >
       {children}
     </div>
